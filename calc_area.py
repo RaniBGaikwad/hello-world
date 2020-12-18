d91ab949fa5944db9b033b55a2a8cd4821eec754
Rani B Gaikwad <ranigaikwad301993@gmail.com>
2020-12-18 13:48:54 +0530

    Update calc_area.py

--- calc_area.py
+++ calc_area.py
@@ -15,4 +15,9 @@ s = (a + b + c) / 2
 # calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 print('The area of the triangle is %0.2f' %area)
+<<<<<<< Updated upstream
 #Output of the above code:
+=======
+
+
+>>>>>>> Stashed changes
